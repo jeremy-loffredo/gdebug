856104d515b5a6383c2c45100bf4d40583954599
Jeremy Loffredo <jeremy.loffredo@lmrc6275.rcoffice.ringcentral.com>
2016-10-18 17:35:21 -0400

    save

--- state_collection.js
+++ state_collection.js
@@ -1,7 +1,27 @@
-console.log('INJECTED');
-Data.state.on('change',function(a) {
-	console.log('========================================');
-	console.log(Data.state.get('version'));
-	console.log(a.get('version'));
-	console.log('========================================');
-});
+var record_states = [];
+
+var record_timeout = null;
+var record = function(a) {
+	if (record_timeout !== null) {
+		record_states.push(a.attributes);
+	}
+};
+
+var start_send_records = function() {
+	record_timeout = setTimeout(function() {
+		var data = $.extend(true,{},record_states);
+		record_states = [];
+		Data.dump_to_server('state_changes',data);
+		record_timeout = setTimeout
+		start_send_records();
+	},30000);
+};
+
+var end_record_collection = function() {
+	clearTimeout(record_timeout);
+	record_timeout = null;
+	Data.state.off('change',record);
+};
+
+start_send_records();
+Data.state.on('change',record);
