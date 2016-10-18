ccd17e9c34a62234df28d55edf8dff49c8f43c35
Jeremy Loffredo <jeremy.loffredo@lmrc6275.rcoffice.ringcentral.com>
2016-10-18 14:49:41 -0400

    Save

---
+++ state_collection.js
@@ -0,0 +1,7 @@
+
+Data.state.on('change',function(a) {
+	console.log('========================================');
+	console.log(Data.state.get('version'));
+	console.log(a.get('version'));
+	console.log('========================================');
+});
